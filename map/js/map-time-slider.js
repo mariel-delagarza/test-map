e321ddef44b84015a0b1f4718f29a9a5bbab847e
Mariel de la Garza <mariel.delagarza@viget.com>
2023-03-15 13:51:02 -0400

    update page break, remove iframe checks

--- map/js/map-time-slider.js
+++ map/js/map-time-slider.js
@@ -442,10 +442,8 @@ function viewLegendHandler() {
   viewLegendButton.classList.add("display-none");
   hideLegendButton.classList.remove("display-none");
 
-  if (typeof mapFrame != "undefined" && mapFrame != null) {
-    viewFullScreenButton.classList.remove("display-block");
-    viewFullScreenButton.classList.add("display-none");
-  }
+  viewFullScreenButton.classList.remove("display-block");
+  viewFullScreenButton.classList.add("display-none");
 }
 
 function hideLegendHandler() {
@@ -455,15 +453,13 @@ function hideLegendHandler() {
   viewLegendButton.classList.remove("display-none");
   viewLegendButton.classList.add("display-block");
 
-  if (typeof mapFrame != "undefined" && mapFrame != null) {
-    viewFullScreenButton.classList.add("display-block");
-  }
+  viewFullScreenButton.classList.add("display-block");
 }
 
 /* ------------------------- Handle Window Resizing ------------------------- */
 
 // window width for using desktop CSS
-const desktop = 1075;
+const desktop = 1000;
 
 // observe window resize
 window.addEventListener("resize", resizeHandler);
@@ -482,34 +478,21 @@ function resizeHandler() {
     viewLegendButton.classList.remove("display-block");
     hideLegendButton.classList.add("display-none");
 
-    /*if (
-      typeof mapFrame != "undefined" &&
-      mapFrame != null &&
-      $("iframe")[0].clientWidth >= desktop
-    ) {
-      console.log("desktop");
-      console.log($("iframe")[0].clientWidth);
-      viewFullScreenButton.classList.remove("full-screen-mobile");
-      viewFullScreenButton.classList.remove("display-none");
-      viewFullScreenButton.classList.add("display-block");
-      viewFullScreenButton.classList.add("iframe");
-    }*/
+    console.log("desktop");
+    viewFullScreenButton.classList.remove("full-screen-mobile");
+    viewFullScreenButton.classList.remove("display-none");
+    viewFullScreenButton.classList.add("display-block");
+    viewFullScreenButton.classList.add("iframe");
   }
 
   if (windowInnerWidth < desktop) {
     viewLegendButton.classList.remove("display-none");
     toolbox.classList.add("display-none");
     console.log("mobile");
-    /*if (
-      typeof mapFrame != "undefined" &&
-      mapFrame != null &&
-      $("iframe")[0].clientWidth < desktop
-    ) {
-      console.log($("iframe")[0].clientWidth);
-      viewFullScreenButton.classList.remove("display-none");
-      viewFullScreenButton.classList.add("display-block");
-      viewFullScreenButton.classList.add("full-screen-mobile");
-      viewFullScreenButton.classList.remove("iframe");
-    }*/
+
+    viewFullScreenButton.classList.remove("display-none");
+    viewFullScreenButton.classList.add("display-block");
+    viewFullScreenButton.classList.add("full-screen-mobile");
+    viewFullScreenButton.classList.remove("iframe");
   }
 }
